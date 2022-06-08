e196fcffa8f99d876e1f8b85672d3782d26fdc52
krypd <krypel.dominik@gmail.com>
2022-06-08 16:34:19 +0200

    ann DoWhile class

---
+++ src/DoWhile.java
@@ -0,0 +1,13 @@
+public class DoWhile {
+
+    public static void main(String[] args) {
+
+        int number = 5;
+
+        do {
+            System.out.println("Moja wartość to " + number + " i jestem mniejszy od 20");
+            number++;
+        }
+        while (number < 20);
+    }
+}
