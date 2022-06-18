ee38c51006c0d4ac52e70b13e2f4af1d9b2ef5cc
dominikkrypel <krypel.dominik@gmail.com>
2022-06-18 23:02:36 +0200

    clean code

--- src/InkrementacjaDekrementacja.java
+++ src/InkrementacjaDekrementacja.java
@@ -2,17 +2,15 @@ public class InkrementacjaDekrementacja {
 
     public static void main(String[] args) {
 
-    int a = 0;
+        int a = 0;
         System.out.println("Wartość pierwsza a " + a);
 
-    int b = a++;
+        int b = a++;
         System.out.println("Wartość druga a " + a);
         System.out.println("Wartość b " + b);
 
-    int c = ++a;
+        int c = ++a;
         System.out.println("Wartość trzecia a " + a);
         System.out.println("Wartość c " + c);
-
-
     }
 }
